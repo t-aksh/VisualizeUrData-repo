d021932d642b08a8ac3498a81991aff0022e60b0
Akash Sharma <t-aksh@microsoft.com>
2020-07-08 10:40:45 -0500

    Update Powershell

--- CreateWebApp/UpdateARMfromConfig.ps1
+++ CreateWebApp/UpdateARMfromConfig.ps1
@@ -1,7 +1,34 @@
 $contents = Get-Content -Raw -Path C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\Config.json | ConvertFrom-Json
 $Endpoints = $contents[0].resources
 
-foreach ($i in $Endpoints)
+$ARMtemplate = Get-Content -Raw -Path C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\ADFTutorialARM.json | ConvertFrom-Json
+$ARMResources = $ARMtemplate.resources.resources
+
+$DataFactoryPipeline =@"
 {
-   Write-Host $i.EndPoint
+  "name": "PushedPipeline",
+  "type": "Microsoft.DataFactory/factories/pipelines"
+}
+"@
+
+Write-Host "Updated Resources"
+
+foreach ($i in $Endpoints)
+{	
+    $PipelineAPIEndpoint = $i.EndPoint
+    Write-Host $PipelineAPIEndpoint
+    $DataFactoryPipeline =@"
+{
+  "name": "PushedPipeline",
+  "type": "Microsoft.DataFactory/factories/pipelines"
+}
+"@
+
+$ARMResources += (ConvertFrom-Json -InputObject $DataFactoryPipeline)				
+}
+
+
+foreach ($i in $ARMResources)
+{	
+   Write-Host $i
 }
