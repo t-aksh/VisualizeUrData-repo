f28299f9c275b2693c3dcbc63fe8530b67e014cc
Akash Sharma <t-aksh@microsoft.com>
2020-07-09 07:09:37 -0500

    Trial Commit

--- CreateWebApp/UpdateARMfromConfig.ps1
+++ CreateWebApp/UpdateARMfromConfig.ps1
@@ -1,34 +1,36 @@
 $contents = Get-Content -Raw -Path C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\Config.json | ConvertFrom-Json
 $Endpoints = $contents[0].resources
 
-$ARMtemplate = Get-Content -Raw -Path C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\ADFTutorialARM.json | ConvertFrom-Json
+$ARMtemplate = ( Get-Content -Raw -Path C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\ADFTutorialARM.json | 
+ConvertFrom-Json -depth 32 )
 $ARMResources = $ARMtemplate.resources.resources
 
+Write-Host "Done"
+ 
+#set-content 'C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\ADFTutorialARM.json'
+#$SomeRandomVar = $ARMtemplate.resources.resources
+#Write-Host $SomeRandomVar
+
 $DataFactoryPipeline =@"
-{
-  "name": "PushedPipeline",
-  "type": "Microsoft.DataFactory/factories/pipelines"
-}
+	{
+	  "name": "PushedPipeline",
+	  "type": "Microsoft.DataFactory/factories/pipelines"
+	}
 "@
 
-Write-Host "Updated Resources"
+#You can use powershell script to deploy templates too :)
+#https://adamtheautomator.com/arm-output-variables-in-azure-pipelines-powershell/#capturing-arm-outputs-for-the-azure-pipeline
 
-foreach ($i in $Endpoints)
-{	
-    $PipelineAPIEndpoint = $i.EndPoint
-    Write-Host $PipelineAPIEndpoint
-    $DataFactoryPipeline =@"
-{
-  "name": "PushedPipeline",
-  "type": "Microsoft.DataFactory/factories/pipelines"
-}
-"@
+#$ARMtemplate.resources | Add-Member -NotePropertyName location -NotePropertyValue $JSONDateTime -Force
+#$JSON.SiteMaintenanceNotificationItems | Add-Member -NotePropertyName StartTime -NotePropertyValue $JSONDateTime -Force
+#$ARMtemplate.resources.resources += (ConvertFrom-Json -InputObject $DataFactoryPipeline)
+#WORKS
 
-$ARMResources += (ConvertFrom-Json -InputObject $DataFactoryPipeline)				
-}
+$ARMtemplate | ConvertTo-Json | set-content 'C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\ADFTutorialARM.json'
 
+$contents[0].resources[0].EndPoint = "Changed EndPoint"
+$contents | ConvertTo-Json | set-content 'C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\Config.json'
 
-foreach ($i in $ARMResources)
-{	
-   Write-Host $i
-}
+#$ARMtemplate.resources.resources 
+#(ConvertFrom-Json -InputObject $DataFactoryPipeline)
+#Write-Host $ARMtemplate.resources.resources
