f59cf1ca160f6d093daa911cd19a7c3d4866a53d
Akash Sharma <t-aksh@microsoft.com>
2020-07-09 09:36:44 -0500

    Trial Commit

--- CreateWebApp/UpdateARMfromConfig.ps1
+++ CreateWebApp/UpdateARMfromConfig.ps1
@@ -6,7 +6,6 @@ ConvertFrom-Json -depth 32 )
 $ARMResources = $ARMtemplate.resources.resources
 
 Write-Host "Done"
- 
 #set-content 'C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\ADFTutorialARM.json'
 #$SomeRandomVar = $ARMtemplate.resources.resources
 #Write-Host $SomeRandomVar
@@ -18,19 +17,7 @@ $DataFactoryPipeline =@"
 	}
 "@
 
-#You can use powershell script to deploy templates too :)
-#https://adamtheautomator.com/arm-output-variables-in-azure-pipelines-powershell/#capturing-arm-outputs-for-the-azure-pipeline
+#$ARMtemplate | ConvertTo-Json | set-content 'C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\ADFTutorialARM.json'
 
-#$ARMtemplate.resources | Add-Member -NotePropertyName location -NotePropertyValue $JSONDateTime -Force
-#$JSON.SiteMaintenanceNotificationItems | Add-Member -NotePropertyName StartTime -NotePropertyValue $JSONDateTime -Force
-#$ARMtemplate.resources.resources += (ConvertFrom-Json -InputObject $DataFactoryPipeline)
-#WORKS
 
-$ARMtemplate | ConvertTo-Json | set-content 'C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\ADFTutorialARM.json'
-
-$contents[0].resources[0].EndPoint = "Changed EndPoint"
-$contents | ConvertTo-Json | set-content 'C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\Config.json'
-
-#$ARMtemplate.resources.resources 
-#(ConvertFrom-Json -InputObject $DataFactoryPipeline)
-#Write-Host $ARMtemplate.resources.resources
+New-AzResourceGroupDeployment -Name MyARMDeployment -ResourceGroupName Intern_Akash_RG -TemplateFile C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\ADFTutorialARM.json -TemplateParameterFile C:\Users\t-aksh\Documents\Internship_project\VizUrDataRepo\CreateWebApp\ADFTutorialARM-Parameters.json
